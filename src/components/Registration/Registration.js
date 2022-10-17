ac1cb272cfca95bea71b03b9c9b4e37223cf897f
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-10-17 14:51:45 +0600

    private route updated and registration page design updated

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -4,7 +4,7 @@ import login from '../../images/Login.jpg';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Swal from 'sweetalert2'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from 'firebase/auth';
@@ -19,15 +19,17 @@ import { AuthContext } from '../UserContext/UserContext';
 const Registration = () => {
 
     const { createUser, signInWithGoogle, signInWithGithub, signInWithFacebook } = useContext(AuthContext);
-    const navigate = useNavigate();
-
+    
     const [success, setSuccess] = useState(false);
     const [visible, setVisible] = useState(false);
     const [passwordError, setPasswordError] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
     const passwordFieldType = visible ? "text" : "password";
     const [alreadyExistUser, setAlreadyExistsUser] = useState('');
 
-
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
 
     const handleRegister = (event) => {
         setSuccess(false);
@@ -36,7 +38,8 @@ const Registration = () => {
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(name, email, password);
+        const confirmPassword = event.target.confirmPassword.value;
+        console.log(name, email, password, confirmPassword);
 
         if (!/.{6}/.test(password)) {
             Swal.fire({
@@ -57,6 +60,17 @@ const Registration = () => {
             setPasswordError('You have to add 1 digit (0-9) at least');
             return;
         }
+
+        if(password !== confirmPassword){
+            setConfirmPasswordError('Password is not matching');
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Password is not matching'
+            });
+            return;
+        }
+
         setPasswordError('')
 
         createUser(email, password)
@@ -64,11 +78,13 @@ const Registration = () => {
                 const user = result.user;
                 console.log("Registered user", user);
                 Swal.fire(
-                    'Good job!',
+                    'Great',
                     'User created successfully!',
                     'success'
                 );
+                setSuccess(true)
                 event.target.reset();
+                navigate(from, {replace: true})
             })
 
             .catch(error => {
@@ -88,7 +104,7 @@ const Registration = () => {
                 const user = result.user;
                 console.log("Registered user By Google", user);
                 Swal.fire(
-                    'Good job!',
+                    'Yayy',
                     'Successful Sign In By Google',
                     'success'
                 );
@@ -158,22 +174,24 @@ const Registration = () => {
 
     return (
         <div className='home-parent-div row row-cols-1 row-cols-lg-2  px-5 container g-col-4 mx-auto mt-4'>
-            <div className='image-div'>
+            <div className='image-div my-auto'>
                 <img className='img-fluid login-image' src={login} alt="" />
             </div>
 
-            <div className=' registration-form-parent-div '>
+            <div className='registration-form-parent-div '>
                 <div className=' form-div mt-4 mx-auto registration-form-container px-4'>
                     {/* <h2 className='text-center'> Bootstrap Registration Form</h2> */}
                     <h4 className='text-primary mt-3 text-center'>New User? Please Register</h4>
 
-                    <Form onSubmit={handleRegister} className=''>
+                    <Form onSubmit={handleRegister} className='only-form'>
 
                         <Form.Group className="mb-1" controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
                             <div>
                                 <div>
-                                    <Form.Control type="text" placeholder="Enter Your Name" name="name" required />
+                                    <Form.Control
+                                        className='name-input-field'
+                                        type="text" placeholder="Enter Name" name="name" required />
                                 </div>
                                 <div className='envelope-icon-div'>
                                     <FontAwesomeIcon className='envelope-icon' icon={faUser}></FontAwesomeIcon>
@@ -200,7 +218,7 @@ const Registration = () => {
 
                             <div className=''>
                                 <div>
-                                    <Form.Control className='password-input-field' type={passwordFieldType} placeholder="Password" name="password" required />
+                                    <Form.Control className='password-input-field' type={passwordFieldType} placeholder="Enter Password" name="password" required />
                                 </div>
 
                                 <div className='envelope-icon-div'>
@@ -215,6 +233,27 @@ const Registration = () => {
                             <p className='text-danger'>{passwordError}</p>
                         }
 
+
+                        <Form.Group className=" password-react-parent-div" controlId="formBasicPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+
+                            <div className=''>
+                                <div>
+                                    <Form.Control className='password-input-field' type={passwordFieldType} placeholder="Confirm Password" name="confirmPassword" required />
+                                </div>
+
+                                <div className='envelope-icon-div'>
+                                    {
+                                        visible ? <FontAwesomeIcon onClick={() => setVisible(!visible)} className='text-dark fw-bold' icon={faEyeSlash}></FontAwesomeIcon> : <FontAwesomeIcon onClick={() => setVisible(!visible)} className='text-dark fw-bold' icon={faEye}></FontAwesomeIcon>
+                                    }
+                                </div>
+                            </div>
+                        </Form.Group>
+
+                        {
+                            <p className='text-danger'>{confirmPasswordError}</p>
+                        }
+
                         {
                             success && <p className='user-added-successfully-text-color'> User Added Successfully</p>
                         }
@@ -226,13 +265,13 @@ const Registration = () => {
 
                     <p className='my-2'>Already have an account? <Link to='/login'>Login</Link></p>
                     <div className='d-flex justify-content-center align-items-center'>
-                        <hr className='w-50 me-2' />
+                        <hr className='w-50 me-3' />
                         <span className='fs-5'>or</span>
-                        <hr className='w-50 ms-2' />
+                        <hr className='w-50 ms-3' />
                     </div>
 
                     <div className='google-github-facebook-button-container text-center mt-3'>
-                        <div className='google-button-container rounded-4 mb-3 '>
+                        <div className='google-button-container rounded-4 mb-3'>
                             <button onClick={handleGoogleSignIn} className='google-button border border-0 btn btn-primary-google w-75'>Google Sign in <Link className='m-1 ms-3' title="Google" target="_blank" role="button"><FaGoogle className='fs-4 google-icon-form'></FaGoogle></Link></button>
                         </div>
 
